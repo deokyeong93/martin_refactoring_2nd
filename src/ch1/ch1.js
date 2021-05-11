f61675256b579f4c892ec5fbd4050e98a3f2f47e
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2021-05-11 23:46:34 +0900

    init : first commit
    Readme는 추후에 작성하고, 책에 나오는 코드는 깃에 기록을 하면서 진행할 생각이다.

---
+++ src/ch1/ch1.js
@@ -0,0 +1,33 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+  const format = new Intl.NumberFormat("en-Us", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let pert of invoice.performances) {
+    const play = plays[performance.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르 : ${paly.type}`);
+    }
+  }
+}
