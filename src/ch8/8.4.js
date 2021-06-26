2a50858d4f06757c4a9cb430946b0e7350091bf3
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2021-06-26 12:50:28 +0900

    Add : 8.4 추가

---
+++ src/ch8/8.4.js
@@ -0,0 +1,22 @@
+function renderPerson(outStream, person) {
+  outStream.write(`<p>${person.name}</p>`);
+  renderPhoto(outStream, person.photo);
+  emitPhotoData(outStream, person);
+  outStream.write(`<p>위치 : ${person.photo.location}</p>\n`);
+}
+
+function listRecentPhotos(outStream, photos) {
+  photos
+    .filter((p) => p.date > recentDateCutoff())
+    .forEach((p) => {
+      outStream.write("<div>\n");
+      emitPhotoData(outStream, photo);
+      outStream.write(`<p>위치 : ${photo.location}</p>\n`);
+      outStream.write("<div>\n");
+    });
+}
+
+function emitPhotoData(outStream, photo) {
+  outStream.write(`<p>제목 : ${photo.title}</p>\n`);
+  outStream.write(`<p>날짜 : ${photo.data.toDateString()}</p>\n`);
+}
