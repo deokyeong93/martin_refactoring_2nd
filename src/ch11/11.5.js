89c28412543b9e17fcbe6f64ed3c30e2c01d50b9
rencarHDH <76925718+rencarHDH@users.noreply.github.com>
2021-07-08 10:30:44 +0900

    Add : 11.5 추가

---
+++ src/ch11/11.5.js
@@ -0,0 +1,27 @@
+class Oreder {
+  discountedPrice(basePrice) {
+    switch(this.discountLevel) {
+      case 1: return basePrice * 0.95;
+      case 2: return basePrice * 0.9;
+    }
+  }
+  
+  get firstPrice() {
+    const basePrice = this.quantity * this.itemPrice;
+    // let discountLevel;
+    // if(this.quantity > 100) discountLevel = 2;
+    // else discountLevel = 1;
+    return this.discountedPrice(basePrice);
+    // return this.discountedPrice(basePrice, this.discountLevel);
+  }
+
+  get finalPrice() {
+    const basePrice = this.quantity * this.itemPrice;
+    return this.discountedPrice(basePrice);
+    // return this.discountedPrice(basePrice, this.discountedPrice);
+  }
+
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+}
